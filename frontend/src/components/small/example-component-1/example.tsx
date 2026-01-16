51cb242bf8f15b194c7e5e9a3882b4e7b2792d87
1mposs1blyt <60925262+1mposs1blyt@users.noreply.github.com>
2026-01-16 16:08:17 +0700

    Переделка файловой структуры, добавлены базовые файлы компонентов и описания как что должно выглядеть и что можно использовать

--- frontend/src/components/small/example-component-1/example.tsx
+++ frontend/src/components/small/example-component-1/example.tsx
@@ -11,15 +11,17 @@ const WelcomeMessage = ({
   isLoggedIn,
 }: WelcomeMessageProps): React.ReactElement => {
   return (
-    <div>
-      {isLoggedIn ? (
-        <h1 className="text-green-500">
-          Welcome back, {name}! You have {count} new messages.
-        </h1>
-      ) : (
-        <h1 className="text-blue-500">Welcome to the site! Please log in.</h1>
-      )}
-    </div>
+    <>
+      <div>
+        {isLoggedIn ? (
+          <h1 className="text-green-500">
+            Welcome back, {name}! You have {count} new messages.
+          </h1>
+        ) : (
+          <h1 className="text-blue-500">Welcome to the site! Please log in.</h1>
+        )}
+      </div>
+    </>
   );
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite"; // Import the plugin
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Add the plugin to the array
+    tailwindcss(),
+    basicSsl(), // Add the plugin to the array
   ],
 });
